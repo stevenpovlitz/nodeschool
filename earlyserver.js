b16f576c8404b743d74266a1f717b6f42e201d29
Steven <steven.povlitz@gmail.com>
2017-05-28 13:48:31 -1000

    lesson 2 done

--- earlyserver.js
+++ earlyserver.js
@@ -1,5 +1,22 @@
-http.createServer(function(request, response) {
-  response.writeHead(200); // status code in header
-  response.write("Hello, this is Patrick Star"); // response body
-  response.end(); // close connection
-}).listen(8080); // listens on port 8080
+var http = require('http');
+var server = http.createServer();
+
+server.on('request', function(request, response) {
+  response.writeHead(200);
+  response.write("Hello, this is dog");
+  response.end();
+});
+
+server.on('request', function(request, response) {
+  console.log("New request coming in...");
+});
+
+server.on('request', function(request, response) {
+  console.log("sharing with user - 'Hello, this is dog'");
+});
+
+server.on('close', function() {
+  console.log("Closing down the server...");
+});
+
+server.listen(8080);
